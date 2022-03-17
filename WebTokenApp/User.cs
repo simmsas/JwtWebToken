caef933c4882242b9f08191b7753d093f1a2d3df
simmsas <simaspajarskas@gmail.com>
2022-03-17 21:16:24 +0200

    JWT Web token

---
+++ WebTokenApp/User.cs
@@ -0,0 +1,9 @@
+﻿namespace WebTokenApp
+{
+    public class User
+    {
+        public string UserName { get; set; } = string.Empty;
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+    }
+}
